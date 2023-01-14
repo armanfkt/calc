ced482c1b7f9571fdeb3f4e8d176c59193582731
armanfkt <armanfakhte@gmail.com>
2023-01-14 15:14:44 +0330

    .calc

---
+++ calc-js.js
@@ -0,0 +1,93 @@
+
+
+const ac = () => {
+   document.getElementById('input').value = "0.0"
+   
+}
+
+
+const c = () => {
+    let len=   document.getElementById('input').lentgh
+    if(len>1){
+        len-1
+    }else{
+        
+        document.getElementById('input').value = "0.0"
+ }
+    
+}
+
+//const seven =() => {
+ //   document.getElementById("input").value = "7"
+//}
+
+const seven =() => {
+    document.getElementById("input").value = "7"
+    let len= document.getElementById("seven").value.lentgh
+    if(len=1){
+        value
+    }else{
+        value+="7"
+    }
+    
+}
+
+const eight =() => {
+    document.getElementById("input").value = "8"
+}
+
+const nine =() => {
+    document.getElementById("input").value = "9"
+}
+
+const four =() => {
+    document.getElementById("input").value = "4"
+}
+
+const five =() => {
+    document.getElementById("input").value = "5"
+}
+
+const six =() => {
+    document.getElementById("input").value = "6"
+}
+
+const one =() => {
+    document.getElementById("input").value = "1"
+}
+
+const tow =() => {
+    document.getElementById("input").value = "2"
+}
+const three =() => {
+    document.getElementById("input").value = "3"
+}
+
+const zero =() => {
+    document.getElementById("input").value = "0"
+}
+
+const zarb =() => {
+    document.getElementById("input").value = "*"
+}
+
+const taqsim =() => {
+    document.getElementById("input").value = "/"
+}
+
+const jam =() => {
+    document.getElementById("input").value = "+"
+}
+
+const tafriq =() => {
+    document.getElementById("input").value = "-"
+}
+
+const darsad =() => {
+    document.getElementById("input").value = "%"
+}
+
+const dot =() => {
+    document.getElementById("input").value = "."
+}
+
